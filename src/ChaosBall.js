4cd306f783685064765c4f81114df3737e0c9d48
ZanderSparrow <sondra.silverhawk@hush.com>
2016-01-18 18:54:22 -0800

    add Chaos Ball

---
+++ src/ChaosBall.js
@@ -0,0 +1,6 @@
+var ChaosBall = function(nothing,nothing2,windowWidth, windowHeight) {
+  MovingBall.call(this, Math.random() * 10, Math.random() * 10, windowWidth, windowHeight);
+};
+
+ChaosBall.prototype = Object.create(MovingBall.prototype);
+ChaosBall.prototype.constructor = ChaosBall;
