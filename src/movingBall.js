296e58d91fbd2c7c85d555bc9206b9a3b63be6d0
ZanderSparrow <sondra.silverhawk@hush.com>
2016-01-19 12:32:46 -0800

    Add beginning of collision action

--- src/movingBall.js
+++ src/movingBall.js
@@ -6,13 +6,27 @@ function MovingBall(windowWidth, windowHeight) {
   this.y = this.top;
   this.windowWidth = windowWidth;
   this.windowHeight = windowHeight;
+  this.radius = 25;
+  // Add position to positions obj as k = "x, y" at center
+  ballPositions[(this.x + this.radius) + ", " + (this.y + this.radius)] = true;
 }
 
 MovingBall.prototype = Object.create(Dancer.prototype);
 
 MovingBall.prototype.move = function() {
-  this.x += this.xSpeed;
-  this.y += this.ySpeed; 
+  var newX = this.x + this.xSpeed;
+  var newY = this.y + this.ySpeed;
+
+  // Calculate center, string coordinates seperated by comma: "3, 4"
+  var center = (this.x + this.randius) + ", " + (this.y + this.radius);
+  // Check positions object for center
+  
+  // else:
+    // Delete old pos from obj
+    // Move
+    this.x = newX;
+    this.y = newY;
+    // Add new pos to obj 
 };
 
 MovingBall.prototype.step = function() {
@@ -20,33 +34,31 @@ MovingBall.prototype.step = function() {
   this.move();
   this.setPosition(this.y, this.x);
   this.$node.css(this.styleSettings);
-  // this.$node.css("left", this.x + "px");
-  // this.$node.css("top", this.y + "px");
   this.velocityChanger();
 };
 
 MovingBall.prototype.velocityChanger = function() {
   if(this.y > this.windowHeight) {
-    this.bounce("y", "b");
+    this.bounce("y", true);
   } else if(this.y < 0) {
-    this.bounce("y", "t");
+    this.bounce("y", false);
   } else if(this.x > this.windowWidth) {
-    this.bounce("x", "b");
+    this.bounce("x", true);
   } else if(this.x < 0) {
-    this.bounce("x", "t");
+    this.bounce("x", false);
   }
 };
 
-MovingBall.prototype.bounce = function(xy, tb) {
+MovingBall.prototype.bounce = function(xy, resist) {
   // takes x or y and top or bottom as params
-  if(xy === "x" && tb == "t") {
+  if(xy === "x" && !resist) {
     this.xSpeed *= -1;
-  } else if(xy === "x" && tb === "b") {
+  } else if(xy === "x" && resist) {
     this.xSpeed *= -0.9;
-  } else if(xy === "y" && tb === "b") {
+  } else if(xy === "y" && resist) {
     this.ySpeed *= -0.9;
     this.y = this.windowHeight;
-  } else if(xy === "y" && tb === "t") {
+  } else if(xy === "y" && !resist) {
     this.ySpeed *= -1;
     this.y = 0;
   }
